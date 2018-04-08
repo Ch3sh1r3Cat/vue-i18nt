67be01044493a2ae4ca45d4cfe86473492efdce9
Ch3sh1r3Cat <ca.leduc@me.com>
2018-04-08 23:38:55 +0200

    Version 0.1.1
    - Compiled index.js with Babel to add compatibility with IE11 or older

--- index.js
+++ index.js
@@ -3,78 +3,78 @@
  * Copyright (c) 2018 Charles-André LEDUC. All rights reserved.
  */
 
-const i18n = {
-  install(Vue, options) {
-    const localeKey = 'locale'
-    var translations = {}
+var i18n = {
+  install: function install(Vue, options) {
+    var localeKey = 'locale';
+    var translations = {};
 
-    var supportedLocales = ['en']
+    var supportedLocales = ['en'];
     if (options) {
       if (options.default) {
-        supportedLocales = [options.default]
+        supportedLocales = [options.default];
       }
     }
 
-    const translate = function(source) {
-      const locale = getLocale()
-      const translation = translations[locale]
+    var translate = function translate(source) {
+      var locale = getLocale();
+      var translation = translations[locale];
       if (translation != null) {
         for (var index in translation) {
-          var element = translation[index]
+          var element = translation[index];
           if (element.source === source) {
-            return element.target
+            return element.target;
           }
         }
       }
-      return source
-    }
+      return source;
+    };
 
-    const addTranslation = function(lang, translation) {
-      supportedLocales.push(lang)
-      translations[lang] = translation
-    }
+    var addTranslation = function addTranslation(lang, translation) {
+      supportedLocales.push(lang);
+      translations[lang] = translation;
+    };
 
-    const getLocale = function() {
+    var getLocale = function getLocale() {
       if (localStorage.getItem(localeKey)) {
-        var storage = localStorage.getItem(localeKey)
+        var storage = localStorage.getItem(localeKey);
         if (storage.length > 2) {
-          storage = storage.substr(0, 2)
+          storage = storage.substr(0, 2);
         }
-        return storage
+        return storage;
       }
 
-      const defaultLang = supportedLocales[0]
+      var defaultLang = supportedLocales[0];
 
-      var language = window.navigator.language || defaultLang
+      var language = window.navigator.language || defaultLang;
       if (language.length > 2) {
-        language = language.substr(0, 2)
+        language = language.substr(0, 2);
       }
 
       if (supportedLocales.indexOf(language) > -1) {
-        localStorage.setItem(localeKey, language)
-        return language
+        localStorage.setItem(localeKey, language);
+        return language;
       }
 
-      localStorage.setItem(localeKey, defaultLang)
-      return language
-    }
+      localStorage.setItem(localeKey, defaultLang);
+      return language;
+    };
 
-    const setLocale = function(locale) {
-      localStorage.setItem(localeKey, locale)
-    }
+    var setLocale = function setLocale(locale) {
+      localStorage.setItem(localeKey, locale);
+    };
 
     Vue.i18n = {
       setLocale: setLocale,
       add: addTranslation
-    }
+    };
 
     Vue.prototype.$i18n = {
       tr: translate,
       getLocale: getLocale
-    }
+    };
 
-    Vue.filter('translate', translate)
+    Vue.filter('translate', translate);
   }
-}
+};
 
-export default i18n
+export default i18n;
